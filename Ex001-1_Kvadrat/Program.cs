6b16881ee46511c9c518a4db6822929c96154f4d
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 12:24:44 +0300

    add Ex 001-1 Квадрат числа версия лектора

---
+++ Ex001-1_Kvadrat/Program.cs
@@ -0,0 +1,10 @@
+﻿if(int.TryParse(Console.ReadLine(), out int i)) // проверка того, что пользователь вводит именно число, TryParse проверит число ли введено
+                                                // out int i - присвоит переменной i значение результата перевода строки в число
+{
+    var sum = Math.Pow(i, 2); //Math.Pow возведет наше i во вторую степень
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine("Вы ввели не целое число");
+}
