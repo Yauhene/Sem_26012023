905b15862e308884dd58718e8f647fad51c8a819
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 11:50:28 +0300

    add Ex 001 Квадрат числа своя версия

--- Ex001_Kvadrat/Program.cs
+++ Ex001_Kvadrat/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("Введите исходное число: ");
+int a = int.Parse(Console.ReadLine()); // Parse() переводит тип string в int
+int result = a * a;
+Console.Write("Квадрат числа ");
+Console.Write(a);
+Console.Write(" равен ");
+Console.WriteLine(result);
+Console.WriteLine(" ");
+Console.WriteLine(" ");
