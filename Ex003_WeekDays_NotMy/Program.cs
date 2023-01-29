599f845e94278ef21f6f210ba42712774a342b9e
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 14:42:23 +0300

    add Дни недели по номерам не моя версия

---
+++ Ex003_WeekDays_NotMy/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача про дни недели, не моя версия
+Console.Write("Введите номер дня недели :");
+if(int.TryParse(Console.ReadLine(), out int i))
+{
+    if(i==1) Console.Write("Понедельник");
+        else if(i==2) Console.Write("Понедельник");
+        else if(i==3) Console.Write("Среда");
+        else if(i==4) Console.Write("Четверг");
+        else if(i==5) Console.Write("Пятница");
+        else if(i==6) Console.Write("Суббота");
+        else if(i==7) Console.Write("Воскресенье");
+            else
+            {
+                 Console.Write("Некорректный номер дня недели");
+            }
+
+    
+}
+
+else
+{
+    Console.Write("Вы ввели некорректный номер :");    
+}
